d7a6ea46ca9255dab0cce94626d542ca221d39da
kalwerz1 <lucas.padilla@outlook.com>
2025-03-07 10:27:17 +0100

    pokedex1

---
+++ database-connection.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Ce fichier permet de définir un objet de connexion $databaseConnection 
+que vous pouvez utiliser dans chaque page qui nécessite de faire une requête au SGBD
+Il suffit d'utiliser l'instruction require_once("database_connection.php") 
+pour que la variable $databaseConnection soit utilisable dans votre page
+*/
+global $databaseConnection;
+
+/* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
+
+$databaseConnection = mysqli_connect(
+    'localhost',
+    "root",
+    "",
+    "pokemon",
+    "3306"
+);
+
+
+
+    if ($result = mysqli_query($databaseConnection, "SELECT * FROM pokemon")) {
+
+  // Seek to row number 15
+  mysqli_data_seek($result,14);
+
+  // Fetch row
+  $row=mysqli_fetch_row($result);
+
+  printf ("ID: %s <br>URL: %s<br>Nom: %s<br>PV: %s<br>PtsAttaque: %s<br>PtsDefense: %s<br>PtsSpecial: %s<br>PtsVitesse: %s<br>idType1: %s\r<br>idType2: %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
+
+  // Free result set
+  mysqli_free_result($result);
+}
+?>
---
+++ list.php
@@ -0,0 +1,43 @@
+<!-- 
+    Ce fichier représente la page de liste de tous les pokémons.
+-->
+<?php
+require_once("head.php");
+?>
+<?php
+$databaseConnection = mysqli_connect(
+    'localhost',
+    "root",
+    "",
+    "pokemon",
+    "3306"
+);
+
+
+
+    if ($result = mysqli_query($databaseConnection, "SELECT * FROM pokemon")) {
+
+  // Seek to row number 15
+  mysqli_data_seek($result,14);
+
+  // Fetch row
+  $row=mysqli_fetch_row($result);
+
+  printf ("ID: %s <br>Nom: %s<br>URL: %s<br>PV: %s<br>PtsAttaque: %s<br>PtsDefense: %s<br>PtsSpecial: %s<br>PtsVitesse: %s<br>DateAjout: %s<br>idType2: %s<br>idType1: %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9],$row[10]);
+  
+  // Free result set
+  mysqli_free_result($result);
+}
+?>
+
+<pre>
+    &lt;
+    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS PAR LEUR NOM.
+    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES 
+    &gt;
+    </pre>
+
+
+<?php
+require_once("footer.php");
+?>
